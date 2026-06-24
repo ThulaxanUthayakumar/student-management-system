8b9ac286d715867e28ddf7007d53f61c92722cdc
ThulaxanUthayakumar <thulaxanuthayakumar@gmail.com>
2026-06-24 20:37:21 +0530

    Day 2 - Professional login page with validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-
-      <div className="d-flex">
-        <Sidebar />
-
-        <div className="p-4 flex-grow-1">
-          <AppRoutes />
-        </div>
-      </div>
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (!username || !password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    if (username === "admin" && password === "123456") {
+      alert("Login Successful");
+      navigate("/dashboard");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
   return (
-    <div className="container mt-5">
-      <h2>Login Page</h2>
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Student Management System</h1>
+
+        <p>Welcome Back 👋</p>
+
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
